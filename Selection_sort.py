1cd6e6d5762b1bc992fb7d059f3bb982464abb28
Yik Lun, Peter,  Chan <cylunhk0709@gmail.com>
2016-06-15 20:50:16 -0700

    finsihed selection sort

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,1 +1,38 @@
-"asdfasdf"
+import sys
+
+def selectionSort(a):
+
+    for i in range(len(a)):
+
+        smallest = sys.maxsize
+
+        smallest_index = 0
+
+        for j in range(i,  len(a)):
+
+            if a[j] < smallest:
+
+                smallest = a[j]
+
+                smallest_index = j
+
+
+        swap(a, i , smallest_index)
+
+    return a
+
+def swap(a , i , j):
+
+    if not a: return
+
+    tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp
+
+tests = []
+tests.append([3, 4,5 ,2, 4, 5, 2,3,4, 3, 5])
+tests.append([1,1,1,1,1,1])
+tests.append([])
+
+for test in tests:
+    assert selectionSort(test) == sorted(test)
